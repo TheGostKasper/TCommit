152ae5168f1999646511a79ca22bcc928f780a00
TheGostKasper <TheGostKasper@gmail.com>
2019-11-01 02:29:46 +0200

    Change API IP

--- app/src/app/services/app.crud.ts
+++ app/src/app/services/app.crud.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CRUDService {
-    ApiUrl = 'http://167.71.200.18:8080';
+    ApiUrl = 'http://67.205.129.78:8080';
     // 'http://iisnode.local.com';
     constructor(
         private http: HttpClient
