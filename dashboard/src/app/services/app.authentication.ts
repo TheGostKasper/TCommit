8def580e0670915d71eed5d2bd13eb2e1c81e61d
TheGostKasper <TheGostKasper@gmail.com>
2019-11-01 02:50:17 +0200

    Change IP Host

--- dashboard/src/app/services/app.authentication.ts
+++ dashboard/src/app/services/app.authentication.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenticationService {
-    ApiUrl = 'http://67.205.129.78:8080';
+    ApiUrl = 'http://167.172.130.1:8080';
     // 'http://iisnode.local.com';
     constructor(
         private http: HttpClient
